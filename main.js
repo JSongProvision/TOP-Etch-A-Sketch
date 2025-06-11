c7e0f6c0b6f7cade14669a63ae4b3e5c018ed76f
JSongProvision <childrenofthefenceunite@gmail.com>
2025-06-11 13:28:22 -0400

    Update layout and start dynamic grid
    Updated the page layout to be more responsive and cleaner. Added button
    and beginning logic for user changing grid size dynamically.

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
-const container = document.querySelector(".container");
+const grid = document.querySelector("#grid");
+grid.addEventListener('mouseover',(event) => {
+    const target = event.target;
+    if(target.classList.contains('square')){
+         target.classList.add("hovered");
+    }  
+});
 
-for(let i = 1; i <= 16; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.id = `square-${i}`;
-    container.appendChild(square);
+const setGridBtn = document.querySelector(".grid-btn");
+const body = document.body;
+
+setGridBtn.addEventListener('click',(event) => {
+    const mask = document.createElement('div');
+    mask.classList.add('dialog-mask');
+
+    body.appendChild(mask);
+});
+
+function createGrid(sideSize) {
+    
+    
+    for(let i = 1; i <= (sideSize*sideSize); i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.id = `square-${i}`;
+        grid.appendChild(square);
+    }
 }
+
+
+createGrid(4);
