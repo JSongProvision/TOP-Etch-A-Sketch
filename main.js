3e6aaed901efc457e1d10dcdc2670f8adbbbab82
JSongProvision <childrenofthefenceunite@gmail.com>
2025-06-12 23:06:02 -0400

    Finish functionality
    Added error handling, and styling. Fixed grid resizing errors

--- main.js
+++ main.js
@@ -19,13 +19,12 @@ setGridBtn.addEventListener('click',(event) => {
     submitBtn.addEventListener('click', resetGrid);
 });
 
-function createGrid(sideSize) {
-    
-    
+function createGrid(sideSize) { 
     for(let i = 1; i <= (sideSize*sideSize); i++) {
         const square = document.createElement("div");
         square.classList.add("square");
         square.id = `square-${i}`;
+        square.style.flexBasis = `calc(100% / ${sideSize})`;
         grid.appendChild(square);
     }
 }
@@ -50,7 +49,8 @@ function createInputDialog() {
 
     const input = document.createElement('input');
     input.classList.add('input');
-    input.placeholder = " # per side";
+    input.placeholder = " # per row";
+    input.name = "gridsize";
     dialog.appendChild(input);
 
     const submitBtn = document.createElement ('button');
@@ -59,11 +59,34 @@ function createInputDialog() {
     dialog.appendChild(submitBtn);
 
     body.appendChild(mask);
+    input.focus();
 }
 
 function resetGrid() {
     const input = document.querySelector('.input');
-    userRowSize = input.value;
+    userRowSize = parseInt(input.value);
+
+    if (isNaN(userRowSize) || userRowSize <= 0 || userRowSize > 100) {
+        const dialog = document.querySelector(".dialog");
+        const input = document.querySelector(".input");
+        const warningMsg = document.querySelector ('.warning');
+        const errorLabel = document.createElement ('label');
+        
+
+        errorLabel.classList.add('error-label');
+        errorLabel.textContent = "Please enter a whole number between 1 and 100 for the grid size.";
+        errorLabel.for = 'gridsize'
+
+        if(!dialog.querySelector('.error-label')){
+            input.after(errorLabel);
+            input.style.borderColor = 'red';
+            warningMsg.remove();
+        }
+
+        input.focus();
+        return;
+    };
+
     grid.replaceChildren();
     createGrid(userRowSize);
     
